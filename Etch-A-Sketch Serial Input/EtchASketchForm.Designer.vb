c085b54a403906da9fe5f4da1ce80990b31bf133
Zac <zac.minidude140@Gmail.com>
2024-10-15 23:21:52 -0600

    Added Picture Box and Buttons to Form

--- Etch-A-Sketch Serial Input/EtchASketchForm.Designer.vb
+++ Etch-A-Sketch Serial Input/EtchASketchForm.Designer.vb
@@ -22,10 +22,57 @@ Partial Class EtchASketchForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.QuitButton = New System.Windows.Forms.Button()
+        Me.EraseButton = New System.Windows.Forms.Button()
+        Me.DrawingPictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'QuitButton
+        '
+        Me.QuitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.QuitButton.Location = New System.Drawing.Point(739, 501)
+        Me.QuitButton.Name = "QuitButton"
+        Me.QuitButton.Size = New System.Drawing.Size(183, 66)
+        Me.QuitButton.TabIndex = 0
+        Me.QuitButton.Text = "Quit"
+        Me.QuitButton.UseVisualStyleBackColor = True
+        '
+        'EraseButton
+        '
+        Me.EraseButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EraseButton.Location = New System.Drawing.Point(540, 501)
+        Me.EraseButton.Name = "EraseButton"
+        Me.EraseButton.Size = New System.Drawing.Size(183, 66)
+        Me.EraseButton.TabIndex = 1
+        Me.EraseButton.Text = "Erase"
+        Me.EraseButton.UseVisualStyleBackColor = True
+        '
+        'DrawingPictureBox
+        '
+        Me.DrawingPictureBox.BackColor = System.Drawing.Color.BlanchedAlmond
+        Me.DrawingPictureBox.Location = New System.Drawing.Point(10, 77)
+        Me.DrawingPictureBox.Name = "DrawingPictureBox"
+        Me.DrawingPictureBox.Size = New System.Drawing.Size(938, 411)
+        Me.DrawingPictureBox.TabIndex = 2
+        Me.DrawingPictureBox.TabStop = False
+        '
+        'EtchASketchForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(954, 579)
+        Me.Controls.Add(Me.DrawingPictureBox)
+        Me.Controls.Add(Me.EraseButton)
+        Me.Controls.Add(Me.QuitButton)
+        Me.Name = "EtchASketchForm"
+        Me.Text = "Etch-A-Sketch"
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents QuitButton As Button
+    Friend WithEvents EraseButton As Button
+    Friend WithEvents DrawingPictureBox As PictureBox
 End Class
--- Etch-A-Sketch Serial Input/EtchASketchForm.vb
+++ Etch-A-Sketch Serial Input/EtchASketchForm.vb
@@ -1,3 +1,12 @@
-﻿Public Class EtchASketchForm
+﻿'Zachary Christensen
+'RCET Advanced Programming
+'10/15/24
+'Serial Input Etch-A-Sketch 
+'https://github.com/Minidude140/Etch-A-Sketch-Serial-Input
+'Reused Code From: https://github.com/Minidude140/Etch-A-Sketch.git
+
+
+
+Public Class EtchASketchForm
 
 End Class
